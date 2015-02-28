eb48f64ab81a7a7ad89eaa259aada39a37a01d00
Viktor Benei <viktor.benei@gmail.com>
2015-02-28 11:11:49 +0100

    more text decoration

--- step.sh
+++ step.sh
@@ -6,11 +6,11 @@ echo
 echo "==> Start"
 
 if [ ! -z "${BASH_SCRIPT_WORKING_DIR}" ] ; then
-	echo "Switching to working directory: ${BASH_SCRIPT_WORKING_DIR}"
+	echo "==> Switching to working directory: ${BASH_SCRIPT_WORKING_DIR}"
 	cd "${BASH_SCRIPT_WORKING_DIR}"
 fi
 
-echo "$ bash $__INPUT_FILE__"
+echo "==> $ bash $__INPUT_FILE__"
 set +e
 bash "$__INPUT_FILE__"
 exit $?
