755b149520666d52327c8296ed7a269d6f60233f
Viktor Benei <viktor.benei@gmail.com>
2015-02-28 11:08:56 +0100

    step.YML + option to set the working directory for the script

--- step.sh
+++ step.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+set -e
+
+if [ ! -z "${BASH_SCRIPT_WORKING_DIR}" ] ; then
+	echo "Switching to working directory: ${BASH_SCRIPT_WORKING_DIR}"
+	cd "${BASH_SCRIPT_WORKING_DIR}"
+fi
+
 echo "$ bash $__INPUT_FILE__"
+set +e
 bash "$__INPUT_FILE__"
+exit $?
