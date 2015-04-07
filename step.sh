d767d4696bfdec09cca288b86dda81690f2cfa93
Viktor Benei <viktor.benei@gmail.com>
2015-04-07 10:09:09 +0200

    "set -e" removed because it caused errors with RVM at `cd`; added script finished with code log message

--- step.sh
+++ step.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-set -e
-
 echo
 echo "==> Start"
 
 if [ ! -z "${BASH_SCRIPT_WORKING_DIR}" ] ; then
 	echo "==> Switching to working directory: ${BASH_SCRIPT_WORKING_DIR}"
 	cd "${BASH_SCRIPT_WORKING_DIR}"
+	if [ $? -ne 0 ] ; then
+		echo " [!] Failed to switch to working directory: ${BASH_SCRIPT_WORKING_DIR}"
+		exit 1
+	fi
 fi
 
+echo
 echo "==> $ bash $__INPUT_FILE__"
-set +e
+echo
 bash "$__INPUT_FILE__"
-exit $?
+script_result=$?
+
+echo
+echo "==> Script finished with exit code: ${script_result}"
+exit ${script_result}
