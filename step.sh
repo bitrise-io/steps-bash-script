44d699dd08dfb2e16864965949f3e8dae966e0be
Viktor Benei <viktor.benei@gmail.com>
2015-07-15 11:59:34 +0200

    renamed the temp script content file var name, for clarity

--- step.sh
+++ step.sh
@@ -16,36 +16,36 @@ fi
 
 set -e
 
-CONFIG_file_pth_to_run="${THIS_SCRIPT_DIR}/._script_cont"
+CONFIG_tmp_script_file_path="${THIS_SCRIPT_DIR}/._script_cont"
 
 if [ ! -z "${content}" ] ; then
 	# if content is defined use it
-	printf "${content}" > "${CONFIG_file_pth_to_run}"
+	printf "${content}" > "${CONFIG_tmp_script_file_path}"
 else
 	# for compatibility
 	#  __INPUT_FILE__ can either be a file path
 	#  or the content of the script
 	if [ -f "${__INPUT_FILE__}" ] ; then
 		# it's a file path - move it to the required path, to run
-		cp "${__INPUT_FILE__}" "${CONFIG_file_pth_to_run}"
+		cp "${__INPUT_FILE__}" "${CONFIG_tmp_script_file_path}"
 	else
 		# not a file path, it's the full content
 		if [ -z "${__INPUT_FILE__}" ] ; then
 			echo " [!] No input / content specified!"
 			exit 1
 		fi
-		printf "${__INPUT_FILE__}" > "${CONFIG_file_pth_to_run}"
+		printf "${__INPUT_FILE__}" > "${CONFIG_tmp_script_file_path}"
 	fi
 fi
 
 set +e
 
 echo
-echo "==> $ bash ${CONFIG_file_pth_to_run}"
+echo "==> $ bash ${CONFIG_tmp_script_file_path}"
 echo
-bash "${CONFIG_file_pth_to_run}"
+bash "${CONFIG_tmp_script_file_path}"
 script_result=$?
-rm "${CONFIG_file_pth_to_run}"
+rm "${CONFIG_tmp_script_file_path}"
 
 echo
 echo "==> Script finished with exit code: ${script_result}"
