a612de1ab445d2c1fe5b1cb85f73bc9ffa4a9b3f
Viktor Benei <viktor.benei@gmail.com>
2015-07-15 11:57:44 +0200

    v2: __INPUT_FILE__ is now deprecated, use content instead

--- step.sh
+++ step.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 echo
 echo "==> Start"
 
@@ -12,11 +14,38 @@ if [ ! -z "${BASH_SCRIPT_WORKING_DIR}" ] ; then
 	fi
 fi
 
+set -e
+
+CONFIG_file_pth_to_run="${THIS_SCRIPT_DIR}/._script_cont"
+
+if [ ! -z "${content}" ] ; then
+	# if content is defined use it
+	printf "${content}" > "${CONFIG_file_pth_to_run}"
+else
+	# for compatibility
+	#  __INPUT_FILE__ can either be a file path
+	#  or the content of the script
+	if [ -f "${__INPUT_FILE__}" ] ; then
+		# it's a file path - move it to the required path, to run
+		cp "${__INPUT_FILE__}" "${CONFIG_file_pth_to_run}"
+	else
+		# not a file path, it's the full content
+		if [ -z "${__INPUT_FILE__}" ] ; then
+			echo " [!] No input / content specified!"
+			exit 1
+		fi
+		printf "${__INPUT_FILE__}" > "${CONFIG_file_pth_to_run}"
+	fi
+fi
+
+set +e
+
 echo
-echo "==> $ bash $__INPUT_FILE__"
+echo "==> $ bash ${CONFIG_file_pth_to_run}"
 echo
-bash "$__INPUT_FILE__"
+bash "${CONFIG_file_pth_to_run}"
 script_result=$?
+rm "${CONFIG_file_pth_to_run}"
 
 echo
 echo "==> Script finished with exit code: ${script_result}"
